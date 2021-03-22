7a1b4bc20f16b87c9107aa527673427b0da489ed
iskakoff <sir.iskakoff@gmail.com>
2021-03-21 20:31:38 -0400

    slicing and assignment to and from scalar

---
+++ src/ndarray_t.h
@@ -0,0 +1,217 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <string>
+#include <vector>
+
+namespace ndarray {
+  template<typename T>
+  struct ndarray_t {
+
+    template<typename...Indices>
+    ndarray_t(size_t d1, Indices...inds) : ndarray_t(std::array<size_t, sizeof...(inds) + 1>{{d1, size_t(inds)...}}) {}
+
+    template<size_t D>
+    ndarray_t(const std::array<size_t, D> &dim) : shape_(dim.begin(), dim.end()),
+                                                  strides_(get_strides(dim)),
+                                                  size_(get_size(dim)), offset_(0),
+                                                  data_(new T[size_], std::default_delete<T[]>()) {}
+
+
+    template<typename...Indices>
+    ndarray_t(ndarray_t<T> &ref, Indices...inds) : ndarray_t(ref, std::array<size_t, sizeof...(inds)>{{size_t(inds)...}}) {}
+
+    template<size_t D>
+    ndarray_t(ndarray_t<T> &ref, const std::array<size_t, D> &inds) : shape_(get_shape(ref, inds)),
+                                                                      strides_(get_strides(shape_)),
+                                                                      size_(get_size(shape_)),
+                                                                      offset_(ref.offset_ + get_offset(ref.strides(), inds)),
+                                                                      data_(ref.data_) {}
+
+
+    /**
+     * Convertion into scalar type
+     *
+     * TODO: Add checks that tensor is zero-dimension.
+     *
+     * @tparam Scalar type of LHS argument
+     * @return value of zero-dimension tensor
+     */
+    template<typename Scalar, typename = typename std::enable_if<std::is_convertible<T, Scalar>::value>::type>
+    operator Scalar() const {
+#ifndef NDEBUG
+      zero_dimension();
+#endif
+      return Scalar(data_.get()[offset_]);
+    }
+
+    /**
+     * Obtain reference to the scalar value of the zero-dimension array
+     *
+     * @return reference to the scalar
+     */
+    operator T &() {
+#ifndef NDEBUG
+      zero_dimension();
+#endif
+      return data_.get()[offset_];
+    }
+
+    operator const T &() const {
+#ifndef NDEBUG
+      zero_dimension();
+#endif
+      return data_.get()[offset_];
+    }
+
+    /**
+     * Assignment
+     */
+    template<typename Scalar, typename = typename std::enable_if<std::is_convertible<Scalar, T>::value>::type>
+    ndarray_t<T> &operator=(const Scalar rhs) {
+#ifndef NDEBUG
+      zero_dimension();
+#endif
+      data_.get()[offset_] = T(rhs);
+      return *this;
+    };
+
+
+    /**
+     * TODO: implement const slicing
+     */
+
+
+    virtual ~ndarray_t() {
+    }
+
+    template<typename...Indices>
+    const T *ref(Indices...inds) const {
+      return &data_.get()[offset_ + get_index(inds...)];
+    }
+
+    template<typename...Indices>
+    T *ref(Indices...inds) {
+      return &data_.get()[offset_ + get_index(inds...)];
+    }
+
+    void set_zero() {
+      std::fill(data_, data_.get() + size_, 0);
+    }
+
+
+    /**
+     * TODO:
+     *
+     * operator() (indices...inds)
+     *    - return ndarray with new shape to be shape[sizeof...(indices):] if sizeof...(indices) < shape or 0
+     *    - implicit conversion between scalar types and ndarray
+     *
+     */
+
+    template<typename...Indices>
+    ndarray_t<T> operator()(Indices...inds) {
+      ndarray_t<T> res(*this, inds...);
+      return res;
+    };
+
+    template<typename...Indices>
+    ndarray_t<const typename std::remove_const<T>::type> operator()(Indices...inds) const {
+
+    };
+
+    // Data accessors
+
+    const std::shared_ptr<T> &data() const {
+      return data_;
+    }
+
+    std::shared_ptr<T> &data() {
+      return data_;
+    }
+
+    size_t size() const {
+      return size_;
+    }
+
+    size_t offset() const {
+      return offset_;
+    }
+
+    const std::vector<size_t> &shape() const {
+      return shape_;
+    }
+
+    const std::vector<size_t> &strides() const {
+      return strides_;
+    }
+
+  private:
+    std::vector<size_t> shape_;
+    std::vector<size_t> strides_;
+    size_t size_;
+    size_t offset_;
+    std::shared_ptr<T> data_;
+
+    template<typename ...Indices>
+    size_t get_index(Indices...inds) const {
+#ifndef NDEBUG
+      if (sizeof...(Indices) > shape_.size())
+        throw std::logic_error("wrong dimensions");
+#endif
+
+      std::array<size_t, sizeof...(Indices)> ind_arr{{size_t(inds)...}};
+#ifndef NDEBUG
+      for (size_t i = 0; i < ind_arr.size(); ++i) {
+        if (ind_arr[i] >= shape_[i])
+          throw std::logic_error(std::to_string(i) + "-th index is larger than its dimension.");
+      }
+#endif
+      std::transform(ind_arr.begin(), ind_arr.end(), strides_.begin(), ind_arr.begin(), std::multiplies<size_t>());
+      size_t ind = std::accumulate(ind_arr.begin(), ind_arr.end(), size_t(0), std::plus<size_t>());
+      return ind;
+    }
+
+    template<typename Container, typename Container2>
+    size_t get_offset(const Container &strides, const Container2 &inds) const {
+      return std::inner_product(inds.begin(), inds.end(), strides.begin(), 0ul);
+    }
+
+    template<typename Container>
+    size_t get_size(const Container &shape) const {
+      return std::accumulate(shape.begin(), shape.end(), 1ul, std::multiplies<size_t>());
+    }
+
+    template<size_t D>
+    std::vector<size_t> get_shape(const ndarray_t<T> &ref, const std::array<size_t, D> &inds) const {
+      std::vector<size_t> shape(ref.shape().size() - D, 0);
+      std::copy(ref.shape_.data() + D, ref.shape_.data() + ref.shape_.size(), shape.data());
+      return shape;
+    }
+
+    template<typename Container>
+    std::vector<size_t> get_strides(const Container &shape) const {
+      std::vector<size_t> str(shape.size());
+      if (shape.size() == 0)
+        return str;
+      str[shape.size() - 1] = 1;
+      for (int k = int(shape.size()) - 2; k >= 0; --k)
+        str[k] = str[k + 1] * shape.data()[k + 1];
+      return str;
+    }
+
+    /**
+     * Check that array is zero-dimension. Throw an exception if it's not.
+     */
+    void zero_dimension() const {
+      if (shape_.size() != 0) {
+        throw std::runtime_error("Array is not directly castable to a scalar. Array dimension is " + std::to_string(shape_.size()));
+      }
+    }
+
+
+  };
+
+}
--- test/ndarray_test.cpp
+++ test/ndarray_test.cpp
@@ -3,13 +3,28 @@
  *
  */
 
+#include <random>
+
 
 #include <catch2/catch.hpp>
-#include <ndarray.h>
+#include <ndarray_t.h>
+#include <complex>
+
+void initialize_array(ndarray::ndarray_t<double> &array) {
+  std::random_device rnd_device;
+  // Specify the engine and distribution.
+  std::mt19937 mersenne_engine{rnd_device()};  // Generates random integers
+  std::uniform_int_distribution<double> dist{0.0, 10.0};
+
+  std::generate(array.data().get(), array.data().get() + array.size(), [&dist, &mersenne_engine]() -> double {
+                  return dist(mersenne_engine);
+                }
+  );
+}
 
 
 TEST_CASE("InitNDArrayTest") {
-  ndarray<double> array(1, 2, 3, 4, 5);
+  ndarray::ndarray_t<double> array(1, 2, 3, 4, 5);
   REQUIRE(array.size() == 1 * 2 * 3 * 4 * 5);
   REQUIRE(array.strides()[0] == 120);
   REQUIRE(array.strides()[1] == 60);
@@ -18,3 +33,55 @@ TEST_CASE("InitNDArrayTest") {
   REQUIRE(array.shape()[1] == 2);
   REQUIRE(array.shape()[3] == 4);
 }
+
+TEST_CASE("SliceNDArrayTest") {
+  ndarray::ndarray_t<double> array(1, 2, 3, 4, 5);
+  initialize_array(array);
+  ndarray::ndarray_t<double> array2(array, 0, 1);
+  REQUIRE(array2.size() == 3 * 4 * 5);
+  REQUIRE(array2.strides()[0] == 20);
+  REQUIRE(array2.strides()[1] == 5);
+  REQUIRE(array2.strides()[2] == 1);
+  REQUIRE(array2.shape()[0] == 3);
+  REQUIRE(array2.shape()[1] == 4);
+  REQUIRE(array2.shape()[2] == 5);
+
+  ndarray::ndarray_t<double> array3 = array2(2);
+
+  REQUIRE(array3.size() == 4 * 5);
+  REQUIRE(array3.strides()[0] == 5);
+  REQUIRE(array3.strides()[1] == 1);
+  REQUIRE(array3.shape()[0] == 4);
+  REQUIRE(array3.shape()[1] == 5);
+
+
+}
+
+TEST_CASE("ScalarNDArrayTest") {
+  ndarray::ndarray_t<double> array(1, 2, 3, 4, 5);
+  initialize_array(array);
+  float value = array(0, 1, 2, 3, 4);
+  std::complex<double> value2 = array(0, 1, 2, 3, 4);
+  REQUIRE((value - value2.real()) == Approx(0.0));
+
+  // take reference to an element
+  double &val = array(0, 1, 2, 3, 4);
+
+  // take a slice
+  ndarray::ndarray_t<double> slice = array(0, 1);
+  // check that value in the slice points to the same data
+  REQUIRE((val - slice(2, 3, 4)) == Approx(0.0));
+  // change value at the reference point
+  val = 3.0;
+  // check that value of the slice has been correctly changed
+  REQUIRE((val - slice(2, 3, 4)) == Approx(0.0));
+
+  double v;
+  REQUIRE_THROWS((v = array(0, 1)));
+
+  array(0, 1, 1, 1, 1) = 33.0;
+  REQUIRE((33.0 - slice(1, 1, 1)) == Approx(0.0));
+
+}
+
+
